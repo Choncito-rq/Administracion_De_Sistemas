14b2f546039df3f3015b5532828117bbe49bb5b6
Chonsi <murolopezjosue48@gmail.com>
2026-02-22 16:06:52 -0700

    Update 1_Windows_Diagnostico.ps1

--- Scripts/1_Windows_Diagnostico.ps1
+++ Scripts/1_Windows_Diagnostico.ps1
@@ -1,1 +1,13 @@
+Write-Host "===== DIAGNOSTICO DEL SISTEMA ====="
+
+Write-Host "Nombre del equipo:"
+hostname
+
+Write-Host ""
+Write-Host "Direcciones IP:"
+ipconfig
+
+Write-Host ""
+Write-Host "Espacio en disco:"
+Get-PSDrive C
 
